b2c5ffc3e948ca3a827d230978d66b173db0429f
Pat Dayton <daytonpe14@gmail.com>
2020-08-27 01:58:56 -0500

    hard code some python

--- run.sh
+++ run.sh
@@ -8,4 +8,17 @@ echo "made it into entrypoint"
 
 ls -l -a
 
-python3 ./updateBadge.py
+# python3 ./updateBadge.py
+PYTHON_CODE=$(cat <<END
+# python code starts here
+import math
+
+for i in range(10):
+    print(i, math.sqrt(i))
+
+# python code ends here
+END
+)
+
+# use the 
+res="$(python3 -c "$PYTHON_CODE")"
